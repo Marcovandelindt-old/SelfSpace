67353fd9ad16b6c67be9625ce8ca3410643406c0
Marco van de Lindt <marcovandelindt@live.nl>
2020-09-02 16:47:30 +0200

    Setup connection with OpenWeather

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,14 +8,33 @@ class Home extends BaseController
      */
     public function index()
     {
+        $weatherData = $this->getWeatherData();
+        
         $data = [
-            'title' => 'Home',
-            'page'  => 'home',
+            'title'       => 'Home',
+            'page'        => 'home',
+            'weatherData' => $weatherData,
         ];
         
         echo view('layouts/header', $data);
         echo view('home/index', $data);
         echo view('layouts/footer', $data);
+    }
+    
+    /**
+     * Get the weather data
+     * 
+     * @return $data
+     */
+    public function getWeatherData()
+    {
+        $apiKey   = 'd7d8762ea563696df7fe7aa835b72245';
+        $url      = 'https://api.openweathermap.org/data/2.5/weather?q=Mijdrecht&appid=' . $apiKey . '&units=metric';
+        
+        $contents = file_get_contents($url);
+        $data     = json_decode($contents);
+        
+        return $data;
     }
 
 }
